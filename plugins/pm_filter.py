71abffda46d1f161ea370e8d8030dd2a6edeca78
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-08-07 09:06:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -430,7 +430,7 @@ async def advantage_spoll_choker(bot, query):
                 reqstr = await bot.get_users(reqstr1)
                 if NO_RESULTS_MSG:
                     await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
-                k = await query.message.edit(script.MVE_NT_FND)
+                k = await query.message.edit(script.I_CUDNT)
                 await asyncio.sleep(60)
                 await k.delete()
 
