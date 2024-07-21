cbd7a8df5438ebf965edfaa532300003549530cf
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2024-07-22 00:39:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,9 +13,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21711251'))
-API_HASH = environ.get('API_HASH', '77bfefd0fe7dfc048c1f85d14c88391c')
-BOT_TOKEN = environ.get('BOT_TOKEN', '6499351249:AAEYuzZIVeP641DsG220G8Ju2NygHalLSxM')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -40,7 +40,7 @@ reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = is_enabled((environ.get('NO_RESULTS_MSG', 'False')), False)
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Testing:212002@testing.7plrokt.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Testing")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Testing')
 
